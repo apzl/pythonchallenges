6c372880e32bfa726e64ebebcb981a783f12fa02
Apsal S Kalathukunnel <47264503+apzl@users.noreply.github.com>
2019-10-27 21:37:02 +0530

    created solution for multiplication-table.py

---
+++ solutions/multiplication-table.py
@@ -0,0 +1,7 @@
+''' Python program to find the
+multiplication table (from 1 to 10)'''
+
+# num = int(input("Display multiplication table of? "))
+
+for i in range(1, 11):
+   print(num,'x',i,'=',num*i)
